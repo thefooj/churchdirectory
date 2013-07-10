75234c5d58c4d0e6f4aa11f313fb830c93db3687
Brian Fujito <bfujito@gmail.com>
2013-07-10 10:25:18 -0400

    sendgrid interface

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Churchdirectory::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =  {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
   config.action_mailer.default_url_options = { :host => "churchdirectory.herokuapp.com", :protocol => 'https' }
 
   # Log the query plan for queries taking more than this (works
